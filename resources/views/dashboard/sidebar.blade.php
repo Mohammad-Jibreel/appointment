c8b52410bd75528fafd845fdd6752b3822822b14
Mohammad-Jibreel <mohammadjibreel912@gmail.com>
2025-03-30 14:56:45 -0700

    test

---
+++ resources/views/dashboard/sidebar.blade.php
@@ -0,0 +1,79 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="dashboard">
+        <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-laugh-wink"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">Admin Panel</div>
+    </a>
+
+    <hr class="sidebar-divider my-0">
+
+    <!-- Dashboard -->
+    <li class="nav-item active">
+        <a class="nav-link" href="dashboard">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span>
+        </a>
+    </li>
+
+    <hr class="sidebar-divider">
+
+    <!-- Products Management -->
+    <li class="nav-item">
+        <a class="nav-link" href="">
+            <i class="fas fa-fw fa-box"></i>
+            <span>Products</span>
+        </a>
+    </li>
+
+    <!-- Categories Management -->
+    <li class="nav-item">
+        <a class="nav-link" href="">
+            <i class="fas fa-fw fa-tags"></i>
+            <span>Categories</span>
+        </a>
+    </li>
+
+
+
+    <!-- Orders Management -->
+    <li class="nav-item">
+        <a class="nav-link" href="">
+            <i class="fas fa-fw fa-shopping-cart"></i>
+            <span>Orders</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="">
+            <i class="fas fa-fw fa-tag"></i>
+            <span>Discount</span>
+        </a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="">
+            <i class="fas fa-fw fa-gem"></i>
+            <span>Accessories</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="">
+            <i class="fas fa-fw fa-star"></i>
+            <span>Review</span>
+        </a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="">
+            <i class="fas fa-fw fa-users"></i> <!-- Icon for users -->
+            <span>User</span>
+        </a>
+    </li>
+
+
+
+
+    <hr class="sidebar-divider d-none d-md-block">
+</ul>
--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/dashboard',function(){
+return view('dashboard.index');
+});
